81b84205bea3c7ef966505ffcbebbe5145954cd3
Qman <nurumaik@gmail.com>
2013-10-18 22:02:44 +0400

    Added copyright headers in source files

--- Game.h
+++ Game.h
@@ -1,3 +1,9 @@
+/*
+Copyright © 2013 Alexander Kumenko <nurumaik@gmail.com>
+This work is free. You can redistribute it and/or modify it under the terms of
+the Do What The Fuck You Want To Public License, Version 2, as published by Sam
+Hocevar. See the COPYING file for more details.
+*/
 #ifndef _GAME_H_
 #define _GAME_H_
 
